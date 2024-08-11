14104abcf7c2fd1adeba87b1ea35743061791d3f
johnbonjour <johnbonjoursignin@outlook.com>
2024-08-11 15:41:20 -0700

    Implemented the design

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,18 @@
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col justify-center">
-    <h1 class="text-green-700 text-5xl text-center">Hello World!</h1>
+  <div class="min-h-screen flex flex-col justify-start relative text-center">
+    <div class="mt-5">
+      <h1 class="font-['Orbitron'] text-4xl text-white font-bold">A.A.</h1>
+      <h4 class="font-['Times_New_Roman'] text-white text-xl">Action Archive</h4>
+    </div>
+    <div class="absolute w-full top-[40vh]">
+      <h1 class="-top-12 text-3xl tracking-wider text-[#f8a72f] font-['Times_New_Roman'] leading-none"><span class="tracking-[0rem]">W</span>ant to know a secret?</h1>
+      <div class="flex flex-col justify-between items-center gap-y-6 text-white mt-4">
+        <button class="text-3xl rounded-xl bg-[#ffb800] h-12 font-['Orbitron'] w-32 font-bold">YES</button>
+        <button class="text-4xl rounded-xl bg-[#ffb800] h-12 font-['Orbitron'] w-32 font-bold">NO</button>
+      </div>
+    </div>
   </div>
 </template>
 
