493fc27745df5aaed1d825e1f66ba27aac5882d2
johnbonjour <johnbonjoursignin@outlook.com>
2024-08-12 21:49:47 -0700

    testing api working

--- api/submit.py
+++ api/submit.py
@@ -1,39 +1,21 @@
-import json
-from http.server import BaseHTTPRequestHandler
 import polars as pl
 from os.path import join
+from flask import Flask, request
+
+app = Flask('vyvy-art-club')
 
 
-class handler(BaseHTTPRequestHandler):
-
-    def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
-        return
-
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-
-        try:
-            json_data: dict = json.loads(post_data)
-
-            keys = ['name', 'grade', 'school', 'email', 'ig_handle', 'referee', 'description']
-
-            if type(json_data) != dict:
-                raise BaseException('Invalid JSON')
-            if not all(key in json_data for key in keys):
-                raise BaseException('Invalid JSON')
-
-            new_data = {key: json_data[key] for key in keys}
+@app.route('/api/submit', methods=['POST'])
+def submit():
+    if request.is_json:
+        data = request.get_json()
+        keys = ['name', 'grade', 'school', 'email', 'ig_handle', 'referee', 'description']
+        if type(data) is dict and all(key in data for key in keys):
+            new_data = {key: data[key] for key in keys}
 
             with open(join('data', 'file.txt'), 'a') as f:
                 df = pl.DataFrame(new_data)
                 df.write_csv(f, include_header=False)
-        except BaseException as e:
-            self.send_error(400)
-            self.end_headers()
-            self.wfile.write(str(e).encode('utf-8'))
+            return "Submitted", 200
 
-        self.send_response(200)
-        self.end_headers()
-
+    return "Bad Request", 400
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/submit': {
+        target: 'localhost:5328/api/submit',
+        changeOrigin: true,
+      }
+    }
+  },
   plugins: [vue()],
 })
