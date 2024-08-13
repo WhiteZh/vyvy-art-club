123d07a5ec3bb9e64cf7bba4828bbc06d70bca43
johnbonjour <johnbonjoursignin@outlook.com>
2024-08-12 20:18:55 -0700

    Implemented the entire outlook design.

---
+++ src/pages/InfoPage.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+import {pagemode} from "../glob.js";
+
+async function submit() {
+  let res = await fetch("/api/submit", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: document.getElementById('name'),
+      grade: document.getElementById('grade'),
+      school: document.getElementById('school'),
+      email: document.getElementById('email'),
+      ig_handle: document.getElementById('ig-handle'),
+      referee: document.getElementById('referee'),
+      description: document.getElementById('description'),
+    }),
+  });
+
+  if (res.ok) {
+    pagemode.value = 'completion';
+  } else {
+    console.log(res.statusText);
+  }
+}
+</script>
+
+<template>
+  <form class="mt-14 flex-grow flex flex-col items-stretch justify-start text-left px-4" action="/api/submit" method="post">
+    <label for="name" class="text-2xl">First, Last Name</label>
+    <input type=text id="name" name="name" placeholder="John Doe"/>
+
+    <label for="grade" class="text-2xl">Grade</label>
+    <select id="grade" name="grade">
+      <option>9</option>
+      <option>10</option>
+      <option>11</option>
+      <option>12</option>
+    </select>
+
+    <label for="school" class="text-2xl">School</label>
+    <input type="text" id="school" name="school" placeholder="Example High School"/>
+
+    <label for="email" class="text-2xl">Email</label>
+    <input type="email" id="email" name="email" placeholder="someone@example.com"/>
+
+    <label for="ig-handle" class="text-2xl">IG Handle</label>
+    <input type="text" id="ig-handle" name="ig-handle" placeholder="@john_doe"/>
+
+    <label for="referee" class="text-2xl">Who referred you?</label>
+    <input type="text" id="referee" name="referee"/>
+
+    <label for="description" class="text-md mt-2">Write a short description on what you are passionate about</label>
+    <textarea id="description" name="description" class="rounded-md flex-grow max-h-56 text-black text-lg px-1.5" placeholder="...."/>
+
+    <button type="submit" class="text-2xl rounded-xl bg-[#ffb800] h-12 font-['Orbitron'] w-32 font-bold mt-5 mx-auto" @click.prevent="submit">Submit</button>
+  </form>
+</template>
+
+<style scoped>
+form>input, form>select {
+  @apply text-lg text-black rounded-md h-8 px-1.5;
+}
+</style>
